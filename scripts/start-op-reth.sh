c2a1a392d3823946ab47406bed844f0dcc4ee111
0xPenryn <ian@world.org>
2025-01-06 19:50:23 -0600

    more config attempts

--- scripts/start-op-reth.sh
+++ scripts/start-op-reth.sh
@@ -30,4 +30,5 @@ exec op-reth node \
   --rollup.sequencer-http=$BEDROCK_SEQUENCER_HTTP \
   --rollup.disable-tx-pool-gossip \
   --enable-discv5-discovery \
-  --discovery.v5.port="${PORT__OP_GETH_P2P:-39393}" \
+  --addr 0.0.0.0 \
+  --port="${PORT__OP_GETH_P2P:-39393}" \
